706a1beb8a55ab5e690aba1a990e16c0379c373f
Caio Enrico Krauss Lima <116699496+CaioEnrico@users.noreply.github.com>
2023-03-18 15:31:17 -0300

    Add files via upload

---
+++ Imobiliaria/Apartamento.java
@@ -0,0 +1,13 @@
+public class Apartamento {
+    String endereco;
+    int numQuartos;
+    double area;
+    double aluguel;
+
+    public Apartamento(String endereco, int numQuartos, double area, double aluguel) {
+        this.endereco = endereco;
+        this.numQuartos = numQuartos;
+        this.area = area;
+        this.aluguel = aluguel;
+    }
+}
